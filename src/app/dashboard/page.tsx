361e82e801b86c0f6efe5982f30a7d0e36155d59
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2024-11-11 08:22:58 +0100

    corrected react-icons import error in dashboard/page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Link from 'next/link'
-import { FaBeer } from "@react-icons/all-files/fa/FaBeer";
+
+import { FaBeer } from "react-icons/fa";
 
 export default function Dashboard() {
     return (
