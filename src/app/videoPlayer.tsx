c50a9e4452ac793b32e954ef34f35a2c90051c93
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2024-11-11 12:45:05 +0100

    .

--- src/app/videoPlayer.tsx
+++ src/app/videoPlayer.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef} from 'react';
 
 const VideoPlayer = () => {
   const videoRef = useRef(null);
