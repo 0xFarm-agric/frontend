37178db11873bdc75fb5ac9553a63fa696d73877
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2024-11-13 17:26:04 +0100

    Added connect wallet

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -7,9 +7,13 @@ export const Navbar = () => {
   return (
     <div className="bg-[#DAE4D5]  text-white py-2 px-6 flex justify-between items-center">
       <img width={100} alt='' src={"/logo.png"} />
+
       <div className="flex space-x-4">
+        <button id="connect-wallet-button" className="px-4  bg-[#3c6454] text-white rounded-xl text-[12px] hover:bg-[#93af1c] hover:text-white focus:outline-none focus:ring-2 focus:ring-green-300">
+          Connect Wallet
+        </button>
         <div className="p-2 cursor-pointer ">
-          <svg  xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#26272F] hover:text-green" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-[#26272F] hover:text-green" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={0.8} d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
           </svg>
         </div>
@@ -19,8 +23,8 @@ export const Navbar = () => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
           </svg>
         </div>
-       
-       
+
+
       </div>
     </div>
   );
