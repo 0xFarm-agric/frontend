e7dd07931ba2770c72e6645e347e942b2fcfb543
Angelicabassey <angelicabassey44@gmail.com>
2024-11-17 09:22:22 +0100

    vertical farm update

---
+++ src/app/components/dashboxColtwo.tsx
@@ -0,0 +1,296 @@
+import Image from 'next/image'
+import { FiUser } from "react-icons/fi";
+import { BsPencil } from "react-icons/bs";
+
+
+
+export default function BoxTwo() {
+    return (
+        <main>
+
+            <div className='flex flex-col md:flex-row'>
+                {/* box 1 */}
+                <div className='flex flex-col w-72 '>
+                    <div className='flex gap-4 top-8 relative px-1'>
+                        <p className='bg-white rounded drop-shadow-2xl px-2'> 74 Days Left</p>
+                        <p className='bg-white rounded drop-shadow-2xl px-2 flex items-center gap-2'><FiUser /> 22k</p>
+
+
+                    </div>
+                    <div className='drop-shadow-xl'>
+                        <Image className=' rounded-t-2xl' src="/plant.png" width={260} height={200} alt="Picture of the author" />
+                        {/* text box */}
+                        <div className='border flex flex-col gap-4 w-[260px]'>
+                            <h3 className='w-64 font-semibold px-1 '>Construction of the Ar-Rahamat mosque in North Gambia</h3>
+
+                            {/* invext box end */}
+
+                            <div className='flex bg-gray-300 h-2 w-60 rounded-2xl '>
+                                <div className='bg-main h-2 w-20 py-1 rounded-2xl'></div>
+                            </div>
+
+                            <div className='px-1'>
+
+                                <div className="border-2  border-main flex gap-2 items-center justify-center w-60- px-4-">
+                                    {/* <Link href='/signup'>Create Account <a className=' text-blue-400' href="#">sign up</a>?</Link> */}
+
+                                    <p className="flex gap-2 justify-center items-center w-40 text-center text-lg font-semibold ">Invest  </p>
+                                    <p className="border border-main h-8  border-grey-400"></p>
+                                    <p><BsPencil /></p>
+
+                                </div>
+                                {/* invext box end */}
+                            </div>
+                            <div>
+                                {/* box content */}
+                                <div className='bg-primaryColor flex justify-around'>
+                                    <div>
+                                        <h3 className='text-center text-sm '>Raised</h3>
+                                        <h3 className='font-semibold'>$150,512</h3>
+
+                                    </div>
+                                    <p className="border h-12  border-gray-400"></p>
+
+                                    <div>
+                                        <h3 className='text-center text-sm '>Goal</h3>
+                                        <h3 className='font-semibold'>$200,000</h3>
+
+                                    </div>
+                                    <p className="border h-12  border-gray-400"></p>
+
+                                    <div>
+                                        <h3 className='text-center text-sm '>Left</h3>
+                                        <h3 className='font-semibold'>$200,000</h3>
+
+                                    </div>
+                                </div>
+                                {/* box content end*/}
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    {/* text box end*/}
+
+
+                </div>
+                {/* box-1 */}
+
+                {/* box 2 */}
+                <div className='flex flex-col w-72 '>
+                    <div className='flex gap-4 top-8 relative px-1'>
+                        <p className='bg-white rounded drop-shadow-2xl px-2'> 74 Days Left</p>
+                        <p className='bg-white rounded drop-shadow-2xl px-2 flex items-center gap-2'><FiUser /> 22k</p>
+
+
+                    </div>
+                    <div className='drop-shadow-xl'>
+                        <Image className=' rounded-t-2xl' src="/taking-care-plants.jpg" width={260} height={200} alt="Picture of the author" />
+                        {/* text box */}
+                        <div className='border flex flex-col gap-4 w-[260px]'>
+                            <h3 className='w-64 font-semibold px-1 '>Construction of the Ar-Rahamat mosque in North Gambia</h3>
+
+                            {/* invext box end */}
+
+                            <div className='flex bg-gray-300 h-2 w-60 rounded-2xl '>
+                                <div className='bg-main h-2 w-20 py-1 rounded-2xl'></div>
+                            </div>
+
+                            <div className='px-1'>
+
+                                <div className="border-2  border-main flex gap-2 items-center justify-center w-60- px-4-">
+                                    {/* <Link href='/signup'>Create Account <a className=' text-blue-400' href="#">sign up</a>?</Link> */}
+
+                                    <p className="flex gap-2 justify-center items-center w-40 text-center text-lg font-semibold ">Invest  </p>
+                                    <p className="border border-main h-8  border-grey-400"></p>
+                                    <p><BsPencil /></p>
+
+                                </div>
+                                {/* invext box end */}
+                            </div>
+                            <div>
+                                {/* box content */}
+                                <div className='bg-primaryColor flex justify-around'>
+                                    <div>
+                                        <h3 className='text-center text-sm '>Raised</h3>
+                                        <h3 className='font-semibold'>$150,512</h3>
+
+                                    </div>
+                                    <p className="border h-12  border-gray-400"></p>
+
+                                    <div>
+                                        <h3 className='text-center text-sm '>Goal</h3>
+                                        <h3 className='font-semibold'>$200,000</h3>
+
+                                    </div>
+                                    <p className="border h-12  border-gray-400"></p>
+
+                                    <div>
+                                        <h3 className='text-center text-sm '>Left</h3>
+                                        <h3 className='font-semibold'>$200,000</h3>
+
+                                    </div>
+                                </div>
+                                {/* box content end*/}
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    {/* text box end*/}
+
+
+                </div>
+                {/* box-2 end */}
+
+                {/* box 3 */}
+                <div className='flex flex-col w-72 '>
+                    <div className='flex gap-4 top-8 relative px-1'>
+                        <p className='bg-white rounded drop-shadow-2xl px-2'> 74 Days Left</p>
+                        <p className='bg-white rounded drop-shadow-2xl px-2 flex items-center gap-2'><FiUser /> 22k</p>
+
+
+                    </div>
+                    <div className='drop-shadow-xl'>
+                        <Image className=' rounded-t-2xl' src="/educate.jpg" width={260} height={200} alt="Picture of the author" />
+                        {/* text box */}
+                        <div className='border flex flex-col gap-4 w-[260px]'>
+                            <h3 className='w-64 font-semibold px-1 '>Construction of the Ar-Rahamat mosque in North Gambia</h3>
+
+                            {/* invext box end */}
+
+                            <div className='flex bg-gray-300 h-2 w-60 rounded-2xl '>
+                                <div className='bg-main h-2 w-20 py-1 rounded-2xl'></div>
+                            </div>
+
+                            <div className='px-1'>
+
+                                <div className="border-2  border-main flex gap-2 items-center justify-center w-60- px-4-">
+                                    {/* <Link href='/signup'>Create Account <a className=' text-blue-400' href="#">sign up</a>?</Link> */}
+
+                                    <p className="flex gap-2 justify-center items-center w-40 text-center text-lg font-semibold ">Invest  </p>
+                                    <p className="border border-main h-8  border-grey-400"></p>
+                                    <p><BsPencil /></p>
+
+                                </div>
+                                {/* invext box end */}
+                            </div>
+                            <div>
+                                {/* box content */}
+                                <div className='bg-primaryColor flex justify-around'>
+                                    <div>
+                                        <h3 className='text-center text-sm '>Raised</h3>
+                                        <h3 className='font-semibold'>$150,512</h3>
+
+                                    </div>
+                                    <p className="border h-12  border-gray-400"></p>
+
+                                    <div>
+                                        <h3 className='text-center text-sm '>Goal</h3>
+                                        <h3 className='font-semibold'>$200,000</h3>
+
+                                    </div>
+                                    <p className="border h-12  border-gray-400"></p>
+
+                                    <div>
+                                        <h3 className='text-center text-sm '>Left</h3>
+                                        <h3 className='font-semibold'>$200,000</h3>
+
+                                    </div>
+                                </div>
+                                {/* box content end*/}
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    {/* text box end*/}
+
+
+                </div>
+                {/* box-3 end */}
+
+                {/* box-4 */}
+                <div className='flex flex-col w-72 '>
+                    <div className='flex gap-4 top-8 relative px-1'>
+                        <p className='bg-white rounded drop-shadow-2xl px-2'> 74 Days Left</p>
+                        <p className='bg-white rounded drop-shadow-2xl px-2 flex items-center gap-2'><FiUser /> 22k</p>
+
+
+                    </div>
+                    <div className='drop-shadow-xl'>
+                        <Image className=' rounded-t-2xl' src="/women-working.jpg" width={260} height={200} alt="Picture of the author" />
+                        {/* text box */}
+                        <div className='border flex flex-col gap-4 w-[260px]'>
+                            <h3 className='w-64 font-semibold px-1 '>Construction of the Ar-Rahamat mosque in North Gambia</h3>
+
+                            {/* invext box end */}
+
+                            <div className='flex bg-gray-300 h-2 w-60 rounded-2xl '>
+                                <div className='bg-main h-2 w-20 py-1 rounded-2xl'></div>
+                            </div>
+
+                            <div className='px-1'>
+
+                                <div className="border-2  border-main flex gap-2 items-center justify-center w-60- px-4-">
+                                    {/* <Link href='/signup'>Create Account <a className=' text-blue-400' href="#">sign up</a>?</Link> */}
+
+                                    <p className="flex gap-2 justify-center items-center w-40 text-center text-lg font-semibold ">Invest  </p>
+                                    <p className="border border-main h-8  border-grey-400"></p>
+                                    <p><BsPencil /></p>
+
+                                </div>
+                                {/* invext box end */}
+                            </div>
+                            <div>
+                                {/* box content */}
+                                <div className='bg-primaryColor flex justify-around'>
+                                    <div>
+                                        <h3 className='text-center text-sm '>Raised</h3>
+                                        <h3 className='font-semibold'>$150,512</h3>
+
+                                    </div>
+                                    <p className="border h-12  border-gray-400"></p>
+
+                                    <div>
+                                        <h3 className='text-center text-sm '>Goal</h3>
+                                        <h3 className='font-semibold'>$200,000</h3>
+
+                                    </div>
+                                    <p className="border h-12  border-gray-400"></p>
+
+                                    <div>
+                                        <h3 className='text-center text-sm '>Left</h3>
+                                        <h3 className='font-semibold'>$200,000</h3>
+
+                                    </div>
+                                </div>
+                                {/* box content end*/}
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                    {/* text box end*/}
+
+
+                </div>
+                {/* box-4 end*/}
+
+
+            </div>
+        </main>
+    );
+};
