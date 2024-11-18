0b7a690ee7de88882f8fa0c84712d3e099a58aa7
Angelicabassey <angelicabassey44@gmail.com>
2024-11-18 15:07:10 +0100

    auth fixed

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,53 +1,59 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { AiOutlineSearch } from "react-icons/ai";
+import { PiHouseLineBold } from "react-icons/pi";
+import { FaRegUser } from "react-icons/fa";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FaPhoneAlt } from "react-icons/fa";
+
+
+
 
 
 
 export default function Login() {
   return (
 
-    <main>
+    <main  className='bg-backgroundColor'>
       <div className='flex flex-col md:flex-row justify-between items-center px-20 py-4'>
         <div>
           <Image src="/logo.png" width={160} height={100} alt="oxFramer" />
         </div>
         <div className='flex items-center gap-6'>
-          <Link className=" border border-main text-center px-4 py-1 rounded-full text-sm  md:text-xl py-9-" href="#signup">Log in</Link>
-          <Link className="border border-main hover:bg-main text-center px-4 py-1  rounded-full text-sm  md:text-xl py-9-" href="#signup">Sign Up</Link>
+          <Link className=" border border-main text-main hover:text-white  hover:bg-main text-center px-4 py-1 rounded-full text-sm  md:text-xl " href="login">Log in</Link>
+          <Link className="border border-main text-white hover:text-backgroundColor bg-main text-center px-4 py-1  rounded-full text-sm  md:text-xl" href="signup">Sign Up</Link>
 
         </div>
       </div>
 
       {/* hero */}
-      <div className='flex flex-col md:flex-row  justify-evenly items-center py-10 '>
+      <div className='flex flex-col md:flex-row  justify-evenly py-10 '>
         <div>
-          <Image src="/Frame-circle.png" width={400} height={100} alt="oxFramer" />
+          <Image src="/Frame-circle.png" width={420} height={100} alt="oxFramer" />
 
         </div>
 
-        <div className='flex flex-col justify-center items-center text-center gap-6'>
+        <div className='flex flex-col justify-center items-center text-center gap-4'>
           <div className='flex flex-col gap-6'>
-            <h1 className='text-xl md:text-3xl'>Welcome to OxFrams</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br /> Dolor architecto odio possimus sapiente ex nulla?</p>
+            <h1 className='text-xl md:text-4xl font-semibold'>Welcome to 0xFrams</h1>
+            <p> Revolutionizing Agriculture with Vertical and <br /> Decentralized Farming through Real World Assets</p>
           </div>
 
 
 
           <div className='flex flex-col gap-4'>
             <div className="relative flex items-center">
-              <AiOutlineSearch className="absolute left-3 text-gray-500" />
+              <PiHouseLineBold className="absolute left-3 text-gray-500" />
               <input
                 type="text"
                 className="pl-10 py-2 flex gap-4 w-72 md:w-80 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
-                placeholder='what type of restuarant your are?'
+                placeholder='Type of farm ?'
               />
             </div>
 
             {/*  */}
             <div className="relative flex items-center">
-              <AiOutlineSearch className="absolute left-3 text-gray-500" />
+              <FaRegUser className="absolute left-3 text-gray-500" />
               <input
                 type="text"
                 className="pl-10 py-2 flex gap-4 w-72 md:w-80 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
@@ -58,7 +64,7 @@ export default function Login() {
             {/*  */}
 
             <div className="relative flex items-center">
-              <AiOutlineSearch className="absolute left-3 text-gray-500" />
+              <MdOutlineMailOutline className="absolute left-3 text-gray-500" />
               <input
                 type="text"
                 className="pl-10 py-2 flex gap-4 w-72 md:w-80 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
@@ -69,7 +75,7 @@ export default function Login() {
             {/*  */}
 
             <div className="relative flex items-center">
-              <AiOutlineSearch className="absolute left-3 text-gray-500" />
+              <FaPhoneAlt className="absolute left-3 text-gray-500" />
               <input
                 type="text"
                 className="pl-10 py-2 flex gap-4 w-72 md:w-80 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
@@ -79,15 +85,22 @@ export default function Login() {
 
             {/*  */}
 
-           
-            <Link className="bg-main text-center text-white px-4- py-2 text-sm- rounded-full" href="#signup">Create Account</Link>
 
-            <Link href='/login'>Already Registered, <a className=' text-blue-400' href="#">sign in</a>?</Link>
+            <Link className="bg-main text-center text-white px-4- py-2 text-sm- rounded-full" href="#signup">Create Account</Link>
+            <p>or</p>
+            <section className="flex justify-center gap-4">
+              <Image src="/Twitter.png" alt={"img"} width={"26"} height={"26"} />
+              <Image src="/Linkedin.png" alt={"img"} width={"26"} height={"26"} />
+              <Image src="/Google.png" alt={"img"} width={"26"} height={"26"} />
+
+            </section>
+            <Link href='/login'>Already Registered, <a className=' text-blue-400' href="#">Login</a>?</Link>
+
 
 
           </div>
         </div>
-        
+
 
       </div>
       {/* hero end */}
