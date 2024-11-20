9cfc415264fae00b84008f915dd534cefc7fabba
Ayo Solomon <48213053+Ayoseun@users.noreply.github.com>
2024-11-20 14:53:20 +0100

    .

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,8 +17,8 @@ export default function Login() {
           <Image src="/logo.png" width={160} height={100} alt="oxFramer" />
         </div>
         <div className='flex items-center gap-6'>
-          <Link className="border border-main text-white hover:text-backgroundColor bg-main text-center px-4 py-1  rounded-full text-sm  md:text-xl" href="/login">Log in</Link>
-          <Link className="border border-main hover:bg-main hover:text-backgroundColor text-center px-4 py-1  rounded-full text-sm  md:text-xl py-9-" href="/signup">Sign Up</Link>
+          <Link className="font-thin border border-main text-white hover:text-backgroundColor bg-main text-center px-4 py-1  rounded-full text-xs  md:text-sm" href="/login">Log in</Link>
+          <Link className="border border-gray-400 hover:bg-main hover:text-backgroundColor text-center px-4 py-1  rounded-full text-xs  md:text-sm py-9-" href="/signup">Sign Up</Link>
 
         </div>
       </div>
@@ -33,7 +33,7 @@ export default function Login() {
         <div className='flex flex-col justify-center items-center text-center gap-6'>
           <div className='flex flex-col gap-4'>
             <h1 className='text-xl md:text-4xl font-semibold'>Welcome to 0xFarms</h1>
-            <p> Revolutionizing Agriculture with Vertical and <br /> Decentralized Farming through Real World Assets</p>
+            <p className='font-thin text-gray-500'> Revolutionizing Agriculture with Vertical and <br /> Decentralized Farming through Real World Assets</p>
           </div>
 
 
@@ -45,7 +45,7 @@ export default function Login() {
               <MdOutlineMail className="absolute left-3 text-gray-500" />
               <input
                 type="text"
-                className="pl-10 py-2 flex gap-4 w-72 md:w-80 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                className="text-xs font-thin pl-10 py-2 flex gap-4 w-72 md:w-80 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
                 placeholder='Email or phone'
               />
             </div>
@@ -53,32 +53,27 @@ export default function Login() {
             {/*  */}
 
             <div className="relative flex items-center">
-              <RiLockPasswordLine className="absolute left-3 text-gray-500" />
+              <RiLockPasswordLine className="absolute left-3 text-gray-400" />
               <input
                 type="text"
-                className="pl-10 py-2 flex gap-4 w-72 md:w-80 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+                className="text-xs font-thin pl-10 py-2 flex gap-4 w-72 md:w-80 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
                 placeholder='Password' />
             </div>
-
 
 
             {/*  */}
 
 
-            <Link className="bg-main text-center text-white px-4- py-2 text-sm- rounded-full" href="signup">Login</Link>
+            <Link className="bg-main text-center text-white px-4- py-2 my-8 text-sm rounded-lg" href="dashboard">Login</Link>
 
             <p>or</p>
             <section className="flex justify-center gap-4">
-              <Image src="/Twitter.png" alt={"img"} width={"26"} height={"26"} />
-              <Image src="/Linkedin.png" alt={"img"} width={"26"} height={"26"} />
-              <Image src="/Google.png" alt={"img"} width={"26"} height={"26"} />
+              <Image src="/Twitter.png" alt={"img"} width={"24"} height={"26"} />
+              <Image src="/Linkedin.png" alt={"img"} width={"24"} height={"26"} />
+              <Image src="/Google.png" alt={"img"} width={"24"} height={"26"} />
 
             </section>
-            <p>Create Account <Link className=' text-blue-400' href='/signup'>sign up</Link>?</p>
-
-            </form>
-            <Link className="text-white text-center  px-4- py-2 bg-main rounded-3xl" href="">Login</Link>
-
+            <p className='text-gray-600 font-thin text-sm'>Don't have an account? <Link className=' text-blue-400 text-sm font-semibold' href='/signup'>sign up</Link></p>
 
           </div>
         </div>
@@ -91,4 +86,3 @@ export default function Login() {
 
   );
 }
-
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -17,11 +17,10 @@ export default function Login() {
     <main  className='bg-backgroundColor'>
       <div className='flex flex-col md:flex-row justify-between items-center px-20 py-4'>
         <div>
-
           <Image src="/logo.png" width={160} height={100} alt="oxFramer" />
         </div>
         <div className='flex items-center gap-6'>
-          <Link className=" border border-main text-main hover:text-white  hover:bg-main text-center px-4 py-1 rounded-full text-sm  md:text-xl " href="login">Log in</Link>
+          <Link className=" border border-main text-main hover:text-white  hover:bg-main text-center px-4 py-1 rounded-full text-xs  md:text-sm " href="login">Log in</Link>
           <Link className="border border-main text-white hover:text-backgroundColor bg-main text-center px-4 py-1  rounded-full text-sm  md:text-xl" href="signup">Sign Up</Link>
 
         </div>
@@ -37,7 +36,7 @@ export default function Login() {
         <div className='flex flex-col justify-center items-center text-center gap-4'>
           <div className='flex flex-col gap-6'>
             <h1 className='text-xl md:text-4xl font-semibold'>Welcome to 0xFarms</h1>
-            <p> Revolutionizing Agriculture with Vertical and <br /> Decentralized Farming through Real World Assets</p>
+            <p className='font-thin text-gray-600 text-sm'> Revolutionizing Agriculture with Vertical and <br /> Decentralized Farming through Real World Assets</p>
           </div>
 
 
@@ -109,88 +108,4 @@ export default function Login() {
     </main>
 
   );
-=======
-            <nav className="flex justify-around bg-white items-center py-2">
-                <div>
-                    <Link href="#">
-                        <Image src="/logo.png" width={160} height={100} alt="" />
-                    </Link>
-                </div>
-
-                <div>
-                    <ul className="flex gap-6">
-                        <li  className="hover:text-main text-[#778599] text-white"><a href="">Home</a></li>
-                        <li  className="hover:text-main"><a href="" id=''>Service</a></li>
-                        <li  className="hover:text-main"><a href="">About</a></li>
-                        <li className="hover:text-main"><a href="">Blog</a></li>
-                        <li  className="hover:text-main"><a href="">Contact us</a></li>
-                    </ul>
-                </div>
-
-                <div className="flex gap-6 text-white-">
-                   
-                    <Link className="bg-main px-8 py-1 text-white hover:border hover:bg-white hover:text-main hover:border-main rounded" href="/login">Login</Link>
-                    <Link className="border border-main hover:bg-main hover:text-white text-black  px-8 py-1 rounded" href="/signup">Sign up</Link>
-                </div>
-
-            </nav >
-
-            {/* <section></section> */}
-
-            < div className="signupHero " >
-                {/*  */}
-                < div className=" flex flex-col justify-center items-center top-12 relative" >
-                    <div className="bg-white rounded-xl px-6 py-6 w-96 flex flex-col gap-6">
-                        <div>
-                            <h1 className="text-3xl text-center">Welcome</h1>
-                            <p>Continue With Your <a className="text-blue-500" href="">Google</a> or <a className="text-blue-500" href="">Connect wallet</a></p>
-                        </div>
-                        <form className="flex flex-col justify-center gap-5" action="">
-                            <div className="flex flex-col gap-2">
-                                <label htmlFor="">Full Name</label>
-                                <input className="border border-main rounded-3xl px-4 py-1" placeholder="Full Name" type="text" />
-                            </div>
-
-                            {/*  */}
-                            <div className="flex flex-col gap-2">
-                                <label htmlFor="">Email Address Or Username</label>
-                                <input className="border border-main rounded-3xl px-4 py-1" placeholder="Full Name or Username" type="text" />
-
-                            </div>
-                            {/*  */}
-                            <div className="flex flex-col gap-2">
-
-                                <label htmlFor="">Phone Number</label>
-                                <input className="border border-main rounded-3xl px-4 py-1" placeholder="+234 00000000000" type="text" />
-
-                            </div>
-                            {/*  */}
-                            {/*  */}
-                            <div className="flex flex-col gap-2">
-                                <label htmlFor="">Password</label>
-                                <input className="border border-main rounded-3xl px-4 py-1" placeholder="Password" type="text" />
-
-                            </div>
-                            {/*  */}
-                            <div className="flex flex-col gap-2">
-                                <label htmlFor="">Confirm Password</label>
-                                <input className="border border-main rounded-3xl px-4 py-1" placeholder="Password" type="text" />
-                            </div>
-
-                            {/*  */}
-                            {/* <button className="text-white  px-8 py-1 bg-main rounded">Login  </button> */}
-                        </form>
-                        <Link className="text-white py-2 bg-main rounded-3xl" href="">Register</Link>
-
-
-                    </div>
-                </div >
-                {/*  */}
-
-            </div >
-
-
-        </div >
-    );
-
 }
